2e2f7a60cbd6123e849c35dded4c3ff41e6a3701
Mochammad Dani <drmochamad7@gmail.com>
2021-08-06 09:44:17 +0700

    remove typo variables

--- Producer.php
+++ Producer.php
@@ -4,7 +4,7 @@ require './Config.php';
 
 class Producer extends Config {
     public function produce($message, $topic) {
-        $kafka_producer_config = new RdKafka\Producer($this->$conf);
+        $kafka_producer_config = new RdKafka\Producer($this->conf);
         $kafka_producer = $kafka_producer_config->newTopic($topic);
         $kafka_producer->produce(RD_KAFKA_PARTITION_UA, 0, $message);
         $kafka_producer->flush($this->timeout);
