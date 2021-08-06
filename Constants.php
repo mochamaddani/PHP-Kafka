87253d1bd2aac66b46a8ba3323a40b311d5fed9e
Mochammad Dani <drmochamad7@gmail.com>
2021-08-06 10:27:17 +0700

    remove composer

--- Constants.php
+++ Constants.php
@@ -1,6 +1,6 @@
 <?php
 class Constants {
-    const BOOTSTRAP_SERVER_VALUE = '103.9.147.137:9092';
+    const BOOTSTRAP_SERVER_VALUE = '<bootstrap server>';
     const TIMEOUT_MS = 1000;
     const AUTOCOMMIT_VALUE_MS = 100;
 }
